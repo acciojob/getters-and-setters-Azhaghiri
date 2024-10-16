6bbc85375a79b7ae942f860f7d4bc55d33c9ac29
Azhaghiri <ganadhan6528@gmail.com>
2024-10-16 19:02:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 //complete this code
-class Person {
-	constructor(name,age){
-		this.name = name;
-		this.age = age;
-	}
-	get name(){
-		return this.name
-	}
-	set age(age){
-		return this.age
-	}
-}
+class Person {constructor(name, age) {
+    this._name = name; // Use private property for name
+    this._age = age;   // Use private property for age
+  }
+
+  // Getter for the name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for the age
+  set age(age) {
+    this._age = age;
+  }
+
+  // Getter for the age (optional)
+  get age() {
+    return this._age;
+  }}
 
 class Student extends Person {
 	study(){
-		console.log(`${this.name} is studdying`)
+		 console.log(`${this.name} is studying`);
 	}
 }
 
-
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.name} is teaching`)
+		console.log(`${this.name} is teaching`);
 	}
 }
 
